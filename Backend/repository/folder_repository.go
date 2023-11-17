c7cf89e8602050f5ae86ad0573b7110138e0798c
ZubairARooghwall <ZubairStudyTech@gmail.com>
2023-11-17 18:58:31 +0430

    Create profile_repository.go and folder_repository.go

---
+++ Backend/repository/folder_repository.go
@@ -0,0 +1,84 @@
+package repository
+
+import (
+	"GoVueracleAlchemy/models"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type FolderRepository struct {
+	DB *sql.DB
+}
+
+func NewFolderRepository(db *sql.DB) *FolderRepository {
+	return &FolderRepository{DB: db}
+}
+
+func (fr *FolderRepository) CreateFolder(folder models.Folder) error {
+	query := "INSERT INTO Folders (FolderName, UserID, CreationTime) VALUES (?, ?, CURRENT_TIMESTAMP)"
+	_, err := fr.DB.Exec(query, folder.FolderName, folder.User)
+	if err != nil {
+		log.Printf("Error creating folder %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (fr *FolderRepository) GetFolderByID(folderID int) (*models.Folder, error) {
+	query := "SELECT * FROM Folders WHERE FolderID = ?"
+	row := fr.DB.QueryRow(query, folderID)
+
+	var folder models.Folder
+	err := row.Scan(&folder.FolderID, &folder.FolderName, &folder.User, &folder.CreationDate)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("folder with ID %d not found", folderID)
+		}
+		log.Printf("Error retrieving folder: %v", err)
+		return nil, err
+	}
+
+	return &folder, nil
+}
+
+func (fr *FolderRepository) GetFoldersByUserID(UserID int) (*[]models.Folder, error) {
+	query := "SELECT * FROM Folders WHERE UserID = ?"
+	rows, err := fr.DB.Query(query, UserID)
+	if err != nil {
+		log.Printf("Error retrieving folders for user: %v", err)
+		return nil, err
+	}
+	rows.Close()
+
+	var folders []models.Folder
+	for rows.Next() {
+		var folder models.Folder
+		err = rows.Scan(&folder.FolderID, &folder.FolderName, &folder.User, &folder.CreationDate)
+		if err != nil {
+			log.Printf("Error scanning folder rows: %v", err)
+			return nil, err
+		}
+
+		folders = append(folders, folder)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("Error iterating over folder rows: %v", err)
+		return nil, err
+	}
+
+	return &folders, nil
+}
+
+func (fr *FileRepository) DeleteFolder(folderID int) error {
+	query := "DELETE FROM Folders WHERE FolderID = ?"
+	_, err := fr.DB.Exec(query, folderID)
+	if err != nil {
+		log.Printf("Error deleting folder: %v", err)
+		return err
+	}
+
+	return nil
+}
---
+++ Backend/repository/profile_repository.go
@@ -0,0 +1,54 @@
+package repository
+
+import (
+	"GoVueracleAlchemy/models"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type ProfileRepository struct {
+	DB *sql.DB
+}
+
+func NewProfileRepository(db *sql.DB) *ProfileRepository {
+	return &ProfileRepository{DB: db}
+}
+
+func (pr *ProfileRepository) GetProfileByID(profileID int) (*models.Profile, error) {
+	query := "SELECT * FROM Profiles WHERE ProfileID = ?"
+	row := pr.DB.QueryRow(query, profileID)
+
+	var profile models.Profile
+	err := row.Scan(&profile.ProfileID, &profile.UserID, &profile.ProfilePicture, &profile.Status, &profile.Biography)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("profile with ID %d not found", profileID)
+		}
+		log.Printf("Error retrieving profile: %v", err)
+		return nil, err
+	}
+
+	return &profile, nil
+}
+
+func (pr *ProfileRepository) CreateProfile(profile models.Profile) error {
+	query := "INSERT INTO Profiles (ProfileID, UserID, ProfilePicture, Status, Biography) VALUES (?, ?, ?, ?, ?)"
+	_, err := pr.DB.Exec(query, profile.ProfileID, profile.User, profile.ProfilePicture, profile.Status, profile.Biography)
+	if err != nil {
+		log.Printf("Error creating profile: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (pr *ProfileRepository) DeleteProfile(profileID int) error {
+	query := "DELETE FROM Profiles WHERE ProfileID = ?"
+	_, err := pr.DB.Exec(query, profileID)
+	if err != nil {
+		log.Printf("Error deleting profile: %v", err)
+	}
+
+	return nil
+}
