eecb92af43e0fced35fba1051e79acf46c4dcd55
ZubairARooghwall <ZubairStudyTech@gmail.com>
2023-11-17 06:35:55 +0430

    Complete user_repository.go file

--- Backend/go.mod
+++ Backend/go.mod
@@ -1,11 +1,5 @@
-module github.com/ZubairARoogwall/GoVueracleAlchemy
+module GoVueracleAlchemy
 
 go 1.21.4
 
-require (
-	golang.org/x/crypto v0.15.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/term v0.14.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-)
+require golang.org/x/crypto v0.15.0
--- Backend/repository/user_repository.go
+++ Backend/repository/user_repository.go
@@ -1,11 +1,15 @@
 package repository
 
 import (
-	"../models"
 	"crypto/rand"
 	"database/sql"
 	"encoding/base64"
+	"fmt"
 	"log"
+
+	"GoVueracleAlchemy/models"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func generateSaltValue() (string, error) {
@@ -26,13 +30,12 @@ func hashPasswordWithSalt(password, salt string) (string, error) {
 
 	combined := append([]byte(password), saltBytes...)
 
-	hashedPassword, err := crypto.bcrypt.GenerateFromPassword(combined, crypto.bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword(combined, bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
 
 	return string(hashedPassword), nil
-
 }
 
 type UserRepository struct {
@@ -44,18 +47,51 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 }
 
 func (ur *UserRepository) CreateUser(user models.User) error {
-	query := "INSERT INTO Users (Username, Email, Password, Salt, Role, Education, CreationTime) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)"
-
 	salt, err := generateSaltValue()
 	if err != nil {
-		log.Printf("Error Failed to generate salt value: %v", err)
+		return fmt.Errorf("failed to generate salt: %v", err)
 	}
 
 	hashedPassword, err := hashPasswordWithSalt(user.Password, salt)
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %v", err)
+	}
+
+	query := "INSERT INTO Users (Username, Email, Password, Salt, Role, Education, CreationTime) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)"
 
 	_, err = ur.DB.Exec(query, user.Username, user.Email, hashedPassword, salt, user.Education)
 	if err != nil {
+		// Log or handle the error appropriately
 		log.Printf("Error creating user: %v", err)
+		return fmt.Errorf("failed to create user: %v", err)
+	}
+
+	return nil
+}
+
+func (ur *UserRepository) GetUserByID(userID int) (*models.User, error) {
+	query := "SELECT * UserID, Username, Email, Password, Salt, Role, Education, CreationTime FROM Users WHERE UserID = :1"
+	row := ur.DB.QueryRow(query, userID)
+
+	var user models.User
+	err := row.Scan(&user.UserID, &user.Username, &user.Email, &user.Password, &user.Salt, &user.UserRole, &user.Education, &user.CreationTime)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("user with ID %d not found", userID)
+		}
+		log.Printf("Error retrieving user: %v", err)
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepository) DeleteUser(userID int) error {
+	query := "DELETE FROM Users WHERE UserID = :1"
+	_, err := ur.DB.Exec(query, userID)
+	if err != nil {
+		log.Printf("Error deleting user: %v", err)
+		return err
 	}
 
 	return nil
